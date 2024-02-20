82466bd1106ff70289b49ef7f3fd9372dabb8c95
Kshitij Verma <kshitijv250@gmail.com>
2024-02-20 12:38:31 +0530

    rollback to previous version

--- decs.d.ts
+++
@@ -1,2 +0,0 @@
-declare module "react-lottie-player";
-declare module "react-scrollama";
--- src/constants/window.ts
+++
@@ -1,5 +0,0 @@
-export const isTouchOrStylusBasedScreen =
-  typeof window !== "undefined"
-    ? window.matchMedia("(hover: none) and (pointer: coarse)").matches ||
-      window.matchMedia("(hover: none) and (pointer: fine)").matches
-    : false;
