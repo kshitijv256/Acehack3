664e4985f57108e0eac491b374505ac7bb70daed
bimlesharma <125790247+bimlesharma@users.noreply.github.com>
2024-02-23 16:42:52 +0000

    Get Involved section update

--- src/pages/GetInvolved.jsx
+++ src/pages/GetInvolved.jsx
@@ -19,7 +19,7 @@ const GetInvolved = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
           <QrBox
             image={cp}
-            name="Call for Community Partner"
+            name="Call for Community & Media Partner"
             role={
                 <a
                   href="https://lu.ma/acehack3-community"
