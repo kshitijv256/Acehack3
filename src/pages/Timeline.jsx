8c82c10764b408bee0b14c213945d84df5843170
Alok Agarwal <alokagarwal629@gmail.com>
2024-03-25 23:42:01 +0530

    Update Timeline.jsx
    some changes in the timeline section

--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -3,20 +3,20 @@ import React, { Fragment } from "react";
 const Timeline = () => {
     const events = [
         //Day 1
-        { direction: "right", time: "09:30 AM", heading: "Check-in" },
+        { direction: "right", time: "09:30 AM", heading: "Check-In Starts" },
         { direction: "left", time: "11:00 AM", heading: "Opening Ceremony" },
         { direction: "right", time: "11:30 AM", heading: "Rajasthan Police Session" },
-        { direction: "left", time: "12:00 PM", heading: "Hackathon starts" },
+        { direction: "left", time: "12:00 PM", heading: "Hackathon Starts" },
         { direction: "right", time: "01:30 PM", heading: "Lunch" },
-        { direction: "left", time: "02:30 PM", heading: "Nordex Session" },
+        { direction: "left", time: "02:30 PM", heading: "Nordek Session" },
         { direction: "right", time: "03:30 PM", heading: "Orkes Session" },
-        { direction: "left", time: "04:10 PM", heading: "Hackquest Session" },
-        { direction: "right", time: "06:30 PM", heading: "Funzone opens" },
-        { direction: "left", time: "08:30 PM", heading: "Dinner + Acehack3.0 unplugged" },
+        { direction: "left", time: "04:10 PM", heading: "HackQuest Session" },
+        { direction: "right", time: "06:30 PM", heading: "FunZone Opens" },
+        { direction: "left", time: "08:30 PM", heading: "Dinner + AceHack UnPlugged" },
         { direction: "right", time: "11:30 PM", heading: "Tech Talk by Aryan Singh" },
         { direction: "left", time: "12:30 AM", heading: "Mentoring Round 1" },
         { direction: "right", time: "01:00 AM", heading: "Midnight Snacks" },
-        { direction: "left", time: "01:30 AM", heading: "Game Zone" },
+        { direction: "left", time: "01:30 AM", heading: "Midnight Games" },
     ];
     const events2 = [
         // Day 2
@@ -27,7 +27,7 @@ const Timeline = () => {
         { direction: "right", time: "01:30 PM", heading: "Lunch" },
         { direction: "left", time: "02:30 PM", heading: "Judging Starts" },
         { direction: "right", time: "06:00 PM", heading: "Closing Ceremony" },
-        { direction: "left", time: "07:00 PM", heading: "Bus (Return)" }
+        { direction: "left", time: "07:00 PM", heading: "Bus Return" }
     ];
 
   return (
